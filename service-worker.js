9c1c8c17be8b5ff500904625b1f79afc5cb1f31e
reneelabutay <reneelabutay@Renees-MacBook-Pro.local>
2020-05-15 00:11:22 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e2f3e763baeef6681722d07f7288f163.js"
+  "/portfolio/precache-manifest.ae7ff4870b6e498c4f81301daeb9eec1.js"
 );
 
 self.addEventListener('message', (event) => {
